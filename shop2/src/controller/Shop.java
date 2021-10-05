d1c827d6200283309c0a22e9a7ca2df095743e87
BanJi <BanJi@211.226.220.215>
2021-10-05 13:21:40 +0900

    12

--- shop2/src/controller/Shop.java
+++ shop2/src/controller/Shop.java
@@ -12,11 +12,33 @@ public class Shop {
 		
 		while(true) {
 			System.out.println("그린shop");
-			System.out.println("1.가입 2.탈퇴 3.로그인 4.로그아웃" + "\n100.관리자 0.종료 ");
+			System.out.println("1.가입  2.로그인" + "\n100.관리자 0.종료 ");
 			System.out.print("메뉴선택 : ");
 			int sel = sc.nextInt();
 			if(sel == 1) {
 				um.join();
+			}
+			else if(sel == 2) {
+				if(um.login()) {
+					loginMenu();
+				}
+			}
+			else if(sel == 100) {
+				
+			}
+			else if(sel == 0) {
+				System.out.println("종료되었습니다.");
+				break;
+			}
+		}
+	}
+	public void loginMenu() {
+		while(true) {
+			System.out.println("1.쇼핑 2.장바구니 3.로그아웃 4.탈퇴");
+			System.out.print("메뉴선택 : ");
+			int sel = sc.nextInt();
+			if(sel == 1) {
+				shopMenu();
 			}
 			else if(sel == 2) {
 				
@@ -27,14 +49,10 @@ public class Shop {
 			else if(sel == 4) {
 				
 			}
-			else if(sel == 100) {
-				
-			}
-			else if(sel == 0) {
-				System.out.println("종료되었습니다.");
-				break;
-			}
 		}
+	}
+	public void shopMenu() {
+		
 	}
 	
 	public void run() {
--- shop2/src/controller/UserManager.java
+++ shop2/src/controller/UserManager.java
@@ -9,12 +9,39 @@ public class UserManager {
 
 	Scanner sc = new Scanner(System.in);
 	ArrayList<User> userList = new ArrayList<User>();
+	int log = -1;
 	
 	public void join() {
 		System.out.println("[가입] id 를 입력하세요.");
 		String id = sc.next();
 		User temp = new User(id, 0);
 		userList.add(temp);
-		System.out.println("[메세지] " + temp.id + "님 가입을 축하합니다.");
+		System.out.println(temp.id + "님 가입을 축하합니다.");
 	}
+	
+	public boolean login() {
+		log = -1;
+		System.out.println("[로그인] id 를 입력하세요.");
+		String id = sc.next();
+		for (int i = 0; i < userList.size(); i++) {
+			if (id.equals(userList.get(i).id)) {
+				log = i;
+				break;
+			}
+		}
+		if (log == -1) {
+			System.out.println("없는 id 입니다.");
+		}
+		else {
+			System.out.println(userList.get(log).id + "님 로그인.");
+			return true;
+		}
+		return false;
+	}
+	
+	public void secession() {
+		System.out.println("[탈퇴] id를 입력하세요.");
+	}
+
+	
 }
