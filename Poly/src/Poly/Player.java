1f80fac507ecb8365e52a9d6508ed870c43d40dd
BanJi <BanJi@220.79.17.250>
2021-10-19 13:20:51 +0900

    10-19-14

--- Poly/src/Poly/Player.java
+++ Poly/src/Poly/Player.java
@@ -1,5 +1,11 @@
 package Poly;
 
-public class Player {
+public class Player extends Unit{
 
+	Player(String na , int max , int pow){
+		super(na, max , pow);
+	}
+	public void skill() {
+		
+	}
 }
--- Poly/src/Poly/UnitBat.java
+++ Poly/src/Poly/UnitBat.java
@@ -1,5 +1,10 @@
 package Poly;
 
 public class UnitBat extends Unit{
-
+	UnitBat(){
+		name = "박쥐";
+	}
+	public void skill() {		
+		System.out.println("적 한명에게 침묵 시전 ");
+	}
 }
